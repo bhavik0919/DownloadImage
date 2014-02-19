922156cf70e1bd3e47accbcd99c52f2d6a73040b
Bhavik <bhavik@Bhaviks-Mac.local>
2014-02-19 17:39:26 +0530

    Initial Commit

---
+++ DownloadImage/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  DownloadImage
+//
+//  Created by Bhavik on 19/02/14.
+//  Copyright (c) 2014 Bhavik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ DownloadImage/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DownloadImage
+//
+//  Created by Bhavik on 19/02/14.
+//  Copyright (c) 2014 Bhavik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
